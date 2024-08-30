b2439d9cf91df452c2f20fde5a8d3b3a165f41e4
rlb913 <rebeccalynnbanning@gmail.com>
2024-08-30 14:21:27 -0500

    Iteration 4 - add stats

--- utils_becca.py
+++ utils_becca.py
@@ -1,16 +1,28 @@
-''' ITERATION 3
+''' ITERATION 4
 
 Module: Banning Analytics - Reusable Module for My Data Analytics Projects
 
 This module provides a simple, reusable foundation for my analytics projects. 
 
 Process:
-In this third iteration, I declare additional variables to show skills with different data types. 
+In this fourth iteration, I introduce some basic statistics using Python.
+    - min() is a built in function to find the smallest value passed in
+    - max() is a built in function to find the largest value passed in
+    - The statistics module offered methods to calculate mean and standard deviation.
 '''
 
 #####################################
-# Declare global variables - keep byline at the end.
-# We will use this information in a smarter byline.
+# Import Modules at the Top
+#####################################
+
+# In Python, we can import modules to add extra tools and fuctions.
+# Below, we're importing:
+# - 'statistics': This gives us tools to calculate things like averages.
+
+import statistics
+
+#####################################
+# Declare global variables
 #####################################
 
 # Boolean variable to indicate if the company has international clients
@@ -36,6 +48,24 @@ client_satisfaction_scores: list = [4.8, 4.6, 4.9, 5.0, 4.7]
 
 # Additional list of floats representing individual employee satisfaction scores
 employee_satisfaction_scores: list = [3.9, 4.2, 4.8, 3.0, 4.4]
+
+#####################################
+# Calculate Basic Statistics
+#   Do this BEFORE we declare the byline
+#   so the values have been calculated
+#   and are ready for use in the byline.
+#####################################
+
+# Calculate basic stats using built-in functions min(), max() and statistics module functions mean() and stdev().
+min_client_score: float = min(client_satisfaction_scores)
+max_client_score: float = max(client_satisfaction_scores)
+mean_client_score: float = statistics.mean(client_satisfaction_scores)
+stdev_client_score: float = statistics.stdev(client_satisfaction_scores)
+
+min_employee_score: float = min(employee_satisfaction_scores)
+max_employee_score: float = max(employee_satisfaction_scores)
+mean_employee_score: float = statistics.mean(employee_satisfaction_scores)
+stdev_employee_score: float = statistics.stdev(employee_satisfaction_scores)
 
 #####################################
 # Declare a global variable named byline.
